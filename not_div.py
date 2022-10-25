c50bf79cd18ab93c2840eee48931c3b5fa3a5c41
Chris B <chrisberry888@gmail.com>
2022-10-25 15:23:40 -0400

    Made not_div file

---
+++ not_div.py
@@ -0,0 +1,14 @@
+def not_div(n):
+    print("partial\ttotal")
+    tot = 0
+    temp = 0
+    for i in range(100):
+        if i%n == 0:
+            tot += temp
+            print("%d\t%d" % (temp, tot))
+            temp = 0
+        else:
+            temp += i
+
+
+not_div(3)
