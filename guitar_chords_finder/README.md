e01a4bc80e43012e346ee9a04d19b3c1ef1daf9b
Chris Berry <chrisberry888@pop-os.mynetworksettings.com>
2025-03-04 16:31:24 -0500

    Created find_all_permutations function in the guitar cords file

---
+++ guitar_chords_finder/README.md
@@ -0,0 +1,7 @@
+I'm using this directory to explore a question in guitar: What are all of the ways to comfortably play every relevant cord? At the start I'll just be focusing on the seven normal seventh chords (seen in chord.py). 
+
+The chords I'm looking for should follow the following criteria:
+1) They should be one of the chords in that chord.py file.
+2) When fingering the chord: each of the four fingers on the fretting hand (index, middle, ring, and pinky) should be either 0 or 1 fret away from it's adjacent finger. This means that a chord can't have the index, middle, and ring fingers on one fret and the pinky finger 2 frets away (for example).
+3) If two fingers are on the same fret, then the higher-up finger should be on the lower string. For example, if a chord has both the middle and ring fingers on the 5th fret, then the middle finger should be on the lower string.
+
---
+++ guitar_chords_finder/guitar_chords_finder_main.py
@@ -0,0 +1,37 @@
+from chord import Chord
+from tuning import Tuning
+
+def find_all_permutations(strings, fingers):
+    '''
+    Returns an array containing all possible permutations of "strings" strings
+    being played using "fingers" fingers.
+    '''
+    permutations = []
+    temp = [1,1,1,1]
+    temp.extend(0 for i in range(strings-fingers))
+    end = [0 for i in range(strings-fingers)]
+    end.extend([1,1,1,1])
+
+    permutations.append(temp.copy())
+    
+    while temp != end:
+        temp[0] += 1
+        for i in range(strings):
+            if temp[i] > 1:
+                temp[i] = 0
+                temp[i+1] += 1
+        if sum(temp) == 4:
+            permutations.append(temp.copy())
+    
+    return permutations
+
+
+def guitar_chords_finder():
+    tuning = Tuning.E_STANDARD.value
+    strings = len(tuning)
+    
+    permutations = find_all_permutations(strings, 4)
+
+
+
+print(find_all_permutations(6, 4))
