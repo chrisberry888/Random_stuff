8fc00ea7aa79f916525a2b03dc0c6a5e81d271ab
Chris Berry <chrisberry888@gmail.com>
2026-01-20 19:48:26 -0500

    Finished what I set out to do, I now have all 9 ways of getting 2026 by summing 7 cubes. I could generalize this further and clean up my code but realistically I probably won't

--- sum_of_cubes.py
+++ sum_of_cubes.py
@@ -3,6 +3,18 @@ I got an email from MathsGear about how 2026 can be written as the sum of 7 cube
 They give one example (2026 = 1000 + 1000 + 8 + 8 + 8 + 1 + 1), then say that finding the remaining
 combinations are left as an exercise to the reader. I am making this program to find these remaining 
 combinations.
+
+UPDATE: 7:46PM
+I did it. The answers are:
+2026 = 1000 + 1000 + 8 + 8 + 8 + 1 + 1
+2026 = 1728 + 216 + 64 + 8 + 8 + 1 + 1
+2026 = 729 + 729 + 512 + 27 + 27 + 1 + 1
+2026 = 1000 + 729 + 216 + 64 + 8 + 8 + 1
+2026 = 1331 + 512 + 64 + 64 + 27 + 27 + 1
+2026 = 1000 + 343 + 216 + 216 + 125 + 125 + 1
+2026 = 729 + 512 + 343 + 343 + 64 + 27 + 8
+2026 = 1000 + 343 + 216 + 216 + 216 + 27 + 8
+2026 = 729 + 729 + 216 + 216 + 64 + 64 + 8
 '''
 
 import sys
@@ -78,26 +90,49 @@ def cycle(current_list):
             return
         
 
+def dot(list1, list2):
+    '''
+    Dot product between list1 and list2.
+
+    Ripped this directly from stackoverflow
+    '''
+    if len(list1) != len(list2):
+        return 0
+    return sum((i[0] * i[1] for i in zip(list1, list2)))
+
+
+def make_strings(big_num, list_of_lists, cubes):
+    final_strings = []
+    for current_list in list_of_lists:
+        answer = f'{big_num} ='
+        for i, cube in reversed(list(enumerate(cubes))):
+            while current_list[i] != 0:
+                answer += f" {cube} +"
+                current_list[i] -= 1
+        final_strings.append(answer[:-2])
+
+    return final_strings
+    
+
 
 def hyperspecific():
+    final_list = []
     cubes = [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 1331, 1728]
-    current_answer = [6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    current_answer = [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     last_answer = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7]
-    # while current_answer != last_answer:
-    #     if sum(current_answer) !=
-    print(current_answer)
-    cycle(current_answer)
-    print(current_answer)
+    while current_answer != last_answer:
+        if dot(cubes, current_answer) == 2026:
+            final_list.append(current_answer.copy())
+        cycle(current_answer)
 
 
-'''
-Input: any integer greater than 1
-Output: a 
-'''
+    for my_string in make_strings(2026, final_list, cubes):
+        print(my_string)
+
+
 
 def main():
     hyperspecific()
-    # get_cube_sums(2026)
 
 if __name__ == "__main__":
     main()
